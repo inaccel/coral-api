73611078ba8c5b61b956657a81656cd82cea00e4
Elias Koromilas <elias.koromilas@gmail.com>
2022-04-22 21:59:08 +0300

    Update Java API dependencies

--- scripts/version/java.sh
+++ scripts/version/java.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+sed --in-place --regexp-extended "0,/'com.inaccel:coral-api:.*'/s/'com.inaccel:coral-api:.*'/'com.inaccel:coral-api:$@'/" \
+	build.gradle
 sed --in-place --regexp-extended "0,/<version>.*<\/version>/s/<version>.*<\/version>/<version>$@<\/version>/" \
 	pom.xml
 
