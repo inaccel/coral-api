a4d546f653b9d9b219b3b801be4070085a24d9b6
Elias Koromilas <elias.koromilas@gmail.com>
2022-08-18 19:29:51 +0300

    Use OpenJDK logo for Java API

--- scripts/version/java.sh
+++ scripts/version/java.sh
@@ -5,5 +5,5 @@ sed --in-place --regexp-extended "0,/'com.inaccel:coral-api:.*'/s/'com.inaccel:c
 sed --in-place --regexp-extended "0,/<version>.*<\/version>/s/<version>.*<\/version>/<version>$@<\/version>/" \
 	pom.xml
 
-sed --in-place --regexp-extended "s/007396(.*)message=.*\&/007396\1message=$@\&/" \
+sed --in-place --regexp-extended "s/ffffff(.*)message=.*\&/ffffff\1message=$@\&/" \
 	README.md
